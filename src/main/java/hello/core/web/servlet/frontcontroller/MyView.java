04939e2ef0e5f52bc3dce6a5e5134f175c64ed2c
iamseung <seesungseok@naver.com>
2024-01-04 17:34:22 +0900

    V2까지 개발 완료

---
+++ src/main/java/hello/core/web/servlet/frontcontroller/MyView.java
@@ -0,0 +1,23 @@
+package hello.core.web.servlet.frontcontroller;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/*
+    View 렌더링 컨트롤러
+ */
+public class MyView {
+    private String viewPath;
+
+    public MyView(String viewPath) {
+        this.viewPath = viewPath;
+    }
+
+    public void render(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
+        dispatcher.forward(request, response);
+    }
+}
